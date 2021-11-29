5e4a7484ea4b52a37b0d7a45f7d5ee9d4278c7e7
fabianojustino <fibianojustino@gmail.com>
2021-11-29 18:49:52 -0300

    add exercicios sobre os asterisco

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/asteriscos.js
@@ -0,0 +1,32 @@
+n=5;
+
+//VARIOS ASTERISCO
+for (let index = 0; index < n; index+=1) {
+ // console.log("*****");  
+}
+
+//console.log("\n");
+
+//TRIANGULO RETANGULO
+let asterisco ="*"
+for (let index = 0; index < n; index+=1) {
+ // console.log(asterisco);
+  asterisco+="*";
+  
+}
+
+let caractere="*";
+let espaco ="";
+
+for (let index = 5; index >= 1 ;index-=1) {
+
+  for (let x = 1; x < index; x+=1){
+      espaco+=" ";   
+  }
+
+  console.log(espaco + caractere);
+
+  espaco= "";
+  caractere+="*";
+  
+}
