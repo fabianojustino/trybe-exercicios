76346aa9f29b102e424fba1a801d77980d40cdd4
fabianojustino <fibianojustino@gmail.com>
2021-12-01 16:52:39 -0300

    solved changelle 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises_4.4..js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises_4.4..js
@@ -1,16 +1,39 @@
-let info={
+let info = {
   personagem: "Margarida",
-  origem : "Pato Donald",
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+let infoTioPatinhas = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain",
+  nota: "Dell's Four Color Comics #178",
+  recorrente: "Sim",
 };
 
 //console.log( "Bem vinda, "+ info.personagem)
 
-info["recorrente"]= "Sim";
+info["recorrente"] = "Sim";
 
 //console.table(info);
 
 for (const key in info) {
-//console.log(key);
-console.log(info[key]);
+  //console.log(key);
+  //console.log(info[key]);
+}
+
+//console.table(infoTioPatinhas)
+
+
+for (const key in info) {
+  if (key==="recorrente") {
+    if(info[key]==="Sim" && infoTioPatinhas[key]==="Sim"){
+      console.log("Ambos recorrentes");
+    }else{
+      console.log(info[key] + " e " + infoTioPatinhas[key]);
+    }
+    
+  } else {
+    console.log(info[key] + " e " + infoTioPatinhas[key]);
+  }
 }
