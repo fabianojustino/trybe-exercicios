0c35a499a06cf0c29390e7e795b0aed815f3ecaa
fabianojustino <fibianojustino@gmail.com>
2021-12-01 21:48:59 -0300

    solved changelle parte || exercise  3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises_4.4_bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises_4.4_bonus.js
@@ -1,10 +1,44 @@
-
 //exercise 1
-function palindromo(word){
-  let compareWord="";
-  for (let index = word.length -1; index >=0; index-=1) {
-    compareWord+= word[index];       
+function palindromo(word) {
+  let compareWord = "";
+  for (let index = word.length - 1; index >= 0; index -= 1) {
+    compareWord += word[index];
   }
   return compareWord === word;
 }
-console.log(palindromo("arara"));
+//console.log(palindromo("arara"));
+
+//exercise 2
+function menor(numeros) {
+  let indice = 0;
+  let maior=numeros[0];
+
+  for (let key in numeros) {  
+    if(numeros[key]> maior){
+      maior= numeros[key];
+      indice= key;
+    } 
+}
+return indice;
+}
+// let result = maior([2, 3, 26, 8, 17, 56]);
+// console.log(result);
+
+
+
+//exercise 3
+function menor(numeros) {
+  let indice = 0;
+  let menor=numeros[0];
+
+  for (let key in numeros) {  
+    if(numeros[key]< menor){
+      menor= numeros[key];
+      indice= key;
+    } 
+}
+return indice;
+}
+
+let result = menor([2, 4, 6, 7, 10, 0, -3]);
+ console.log(result);
