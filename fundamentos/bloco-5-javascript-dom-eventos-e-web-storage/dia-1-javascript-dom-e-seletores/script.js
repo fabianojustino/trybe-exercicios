be74ffe005b8c43ecf8cfd5fe57455c4571b7604
fabianojustino <fibianojustino@gmail.com>
2021-12-06 12:17:39 -0300

    Alteado a cor de fundo da div main-content para o verde da trybe

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,10 +1,16 @@
 function setP(description){
-
 let p =document.getElementsByTagName("p");
-
 p[1].innerText =description;
-
 }
+
+
+function setBackroundColorDv(color){
+  let divisoria =document.getElementsByClassName("main-content");
+  divisoria[0].style.backgroundColor = color;
+  
+}
+
+setBackroundColorDv("rgb(76,164,109");
 
 let text= "Eu me vejo como um programador experiente," +
 "participando de diversos projetos";
