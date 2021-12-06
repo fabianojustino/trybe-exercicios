1aa136dcadda6907f7f426cb155f2b94e40fac95
fabianojustino <fibianojustino@gmail.com>
2021-12-06 12:24:05 -0300

    Alterado a cor de fundo da div center-content para branco

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -2,16 +2,24 @@ function setP(description){
 let p =document.getElementsByTagName("p");
 p[1].innerText =description;
 }
-
+let text= "Eu me vejo como um programador experiente," +
+"participando de diversos projetos";
+setP(text);
 
 function setBackroundColorDv(color){
   let divisoria =document.getElementsByClassName("main-content");
   divisoria[0].style.backgroundColor = color;
   
 }
-
 setBackroundColorDv("rgb(76,164,109");
 
-let text= "Eu me vejo como um programador experiente," +
-"participando de diversos projetos";
-setP(text);
+
+function setBackroundColorCenter(color){
+  let divisoria =document.getElementsByClassName("center-content");
+  divisoria[0].style.backgroundColor = color;
+  
+}
+setBackroundColorCenter("white");
+
+
+l
