2fcab8fe421b55840c9243158223c8a3b0bcbd9c
fabianojustino <fibianojustino@gmail.com>
2021-12-09 13:05:48 -0300

    add classe para facilitar a inserção de dias no calendario e uma funcao para acrescentar uma classe ao elemento

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -3,6 +3,14 @@ const weekDaysList = document.querySelector('.week-days');
 createDaysOfTheWeek(weekDaysList, weekDays);
 
 
+const dezDaysList = [29, 30, 1, 2, 
+  3, 4, 5, 6, 7,8, 9, 10, 11, 12,
+   13, 14, 15, 16,17, 18, 19, 20, 21, 22, 23, 24, 
+   25,26, 27, 28, 29, 30, 31];
+   const days = document.querySelector("#days")
+
+  createDaysOfTheWeek(days, dezDaysList, 'day');
+  
 
 function createDaysOfTheWeek(element, arrayDays, classe) {
 
@@ -10,11 +18,17 @@ function createDaysOfTheWeek(element, arrayDays, classe) {
     const days = arrayDays[index];
     const dayListItem = document.createElement('li');
     dayListItem.innerHTML = days;
+
+      //DEFINI UM CLASSE SE SOLICITADO
+      if(typeof(classe)!== undefined){addClassElement(dayListItem, classe)}
  
     element.appendChild(dayListItem);
   };
 };
 
+function addClassElement(element,classe){
+  element.className = classe;
+}
 
 
 
