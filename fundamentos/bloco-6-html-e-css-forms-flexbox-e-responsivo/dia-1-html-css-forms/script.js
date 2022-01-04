22e571f4d40fce90129b6dba844f8d5cb9e20df0
fabianojustino <fibianojustino@gmail.com>
2022-01-04 18:27:55 -0300

    adicionado uma funcao para verificar se os campos estao vazio e outra para escrever dentro de uma div os valores digitados

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,5 +1,5 @@
-const SELECT_ESTADO = document.querySelector("#estado");
-const ARRAY_ESTADOS = [
+const selected_estado = document.querySelector("#estado");
+const array_estados = [
   "AC, Acre",
   "AL, Alagoas",
   "AP, Amapá",
@@ -28,25 +28,68 @@ const ARRAY_ESTADOS = [
   "SE, Sergipe",
   "TO, Tocantins",
 ];
+const submit = document.querySelector("#submit");
+
+// campos requeridos
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const cpf = document.getElementById("cpf");
+const endereco = document.getElementById("endereco");
+const cidade = document.getElementById("cidade");
+
+const answers = document.querySelector("#answers");
 
 function fillStates(states) {
- 
   for (let index = 0; index < states.length; index += 1) {
-    const valueState = states[index].split(',');
-    const sigla = valueState[0].trim()
-    const estado = valueState[1].trim()
+    const valueState = states[index].split(",");
+    const sigla = valueState[0].trim();
+    const estado = valueState[1].trim();
 
     const option = document.createElement("option");
     option.value = sigla;
     option.text = estado;
 
-    if(sigla === 'SP'){
+    if (sigla === "SP") {
       option.selected = true;
     }
 
-    SELECT_ESTADO.appendChild(option);
-
+    selected_estado.appendChild(option);
   }
 }
 
-fillStates(ARRAY_ESTADOS)
+fillStates(array_estados);
+
+function sendForm(event) {
+  event.preventDefault();
+
+  checkEmptyFields();
+}
+
+function writeValues(matriz) {
+  answers.childNodes.remove;
+
+  for (let index = 0; index < matriz.length; index++) {
+    const key = Object.keys(matriz[index]);
+    const value = Object.values(matriz[index]);
+
+    const element = document.createElement("p");
+    element.innerText = `${key} : ${value}`;
+    answers.appendChild(element);
+  }
+}
+
+function checkEmptyFields() {
+  const answersList = [];
+
+  // VALIDA NOME
+  if (nome.value == "" && nome.value.length < 4) {
+    alert("nome invalido");
+  } else {
+    answersList.push({ nome: nome.value });
+  }
+  
+
+  if(answersList.length > 0) writeValues(answersList);
+}
+
+submit.addEventListener("click", sendForm);
