a63c160dc9c2874acd56264890a18008a371eb3e
fabianojustino <fibianojustino@gmail.com>
2022-01-14 22:00:08 -0300

    add dia-01-es6

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte3.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte3.js
@@ -12,19 +12,27 @@ const fatorial = (n) => {
 // console.log(`O fatorial de ${4} é ${fatorial(4)}`);
 
 function fatorialTernario(n) {
-  if (n == 0) {
-    return 1;
-  } else {
-    return n * fatorialTernario(n - 1);
-  }
-}
-// console.log(fatorialTernario(4));
 
+console.log(n, 'n');
+
+let res=0;
+
+if (n == 1) {
+    res = 1;
+    console.log('1' ,'uma vez');
+  } else {
+    res= n * fatorialTernario(n - 1); // 2*1 retorno, res = 2
+    console.log(n, 'n' , res, 'res');
+  }
+  
+  return res;
+}
+
+ fatorialTernario(3);
 
 //recursiva com operador ternario
 const fator = number => number > 1 ? number * fator(number-1): 1 ;
-console.log(fator(4));
-
+// console.log(fator(4));
 
 // Maior palavra de um texto
 const maiorPalavra = (texto) => {
