5ad1d4141d395dbc408b018e9c37297d2101df51
fabianojustino <fibianojustino@gmail.com>
2022-01-16 11:00:20 -0300

    Add part 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
@@ -15,4 +15,15 @@ const myRemove = (array, item) => {
 };
 
 
-module.exports = { sum, myRemove };
+const myFizzBuzz=(num)=> {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+
+
+module.exports = { sum, myRemove, myFizzBuzz };
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,38 +1,43 @@
+const { sum, myRemove, myFizzBuzz } = require("./sum");
 
-const {sum, myRemove} = require('./sum');
 
+/* SUM */
 
-describe('module sum parte 1',()=> {
+describe("module sum parte 1", () => {
+  test("sum of 4 e 5 = 9", () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+  test("sum of 0 e 0 = 0", () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+  test("is not a number, throw an error", () => {
+    expect(() => {
+      sum(4, "5");
+    }).toThrow();
+  });
 
-test('sum of 4 e 5 = 9',()=>{expect(sum(4,5)).toBe(9)});
-test('sum of 0 e 0 = 0',()=>{expect(sum(0,0)).toBe(0)});
-test('is not a number, throw an error',()=>{
-  expect(()=>{sum(4,'5')}).toThrow();
+  test("parameters must be numbers", () => {
+    expect(() => {
+      sum(4, "5");
+    }).toThrowError("parameters must be numbers");
+  });
 });
 
-test('parameters must be numbers',()=>{
-  expect(()=>{sum(4,'5')}).toThrowError('parameters must be numbers');
+/* MY REMOVE */
+
+describe("module myRemove", () => {
+  test("remove the number 1, expects[2,3,4] ", () => {
+    const array = [1, 2, 3, 4];
+    expect([2, 3, 4]).toEqual(myRemove(array, 1));
+  });
+
+  test("remove the number 3, expects[1,2,4] ", () => {
+    const array = [1, 2, 3, 4];
+    expect([1, 2, 4]).toEqual(myRemove(array, 3));
+  });
+
+  test("remove the number 5, expects[1,2,3,4] ", () => {
+    const array = [1, 2, 3, 4];
+    expect([1, 2, 3, 4]).toEqual(myRemove(array, 5));
+  });
 });
-})
-
-
-describe('module myRemove',()=>{
-
-test('remove the number 1, expects[2,3,4] ',()=>{
-  const array = [1,2,3,4];
-  expect([2,3,4]).toEqual(myRemove(array,1));
-})
-
-
-test('remove the number 3, expects[1,2,4] ',()=>{
-  const array = [1,2,3,4];
-  expect([1,2,4]).toEqual(myRemove(array,3));
-})
-
-
-test('remove the number 5, expects[1,2,3,4] ',()=>{
-  const array = [1,2,3,4];
-  expect([1,2,3,4]).toEqual(myRemove(array,5));
-})
-
-})
