188d2878a29e457f39384cecc1d7cad91fe360ce
fabianojustino <fibianojustino@gmail.com>
2022-01-21 22:04:32 -0300

    exercise1.js 2- Retorne o nome do livro de menor nome.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
@@ -1,69 +1,87 @@
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
+      name: "George R. R. Martin",
       birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
+      name: "Stephen King",
       birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
   },
 ];
 
-
-const authorBornIn1947 = (array, year) => array.find((write)=> write.author.birthYear === year);  
+const authorBornIn1947 = (array, year) =>
+  array.find((write) => write.author.birthYear === year);
 
 // Adicione o código do exercício aqui:
-const writer =authorBornIn1947(books, 1947)
-console.log(writer)
+const writer = authorBornIn1947(books, 1947);
+/* console.log(writer) */
+
+// --------------------FOR EACH
+
+function smallerName() {
+  let small = books[0].name.length;
+  let nameBook = books[0].name;
+
+  books.forEach((element) => {
+    const sizeElement = element.name.length;
+    if (sizeElement < small) {
+      small = sizeElement;
+      nameBook = element.name;
+    }
+  });
+  return nameBook;
+}
+
+console.log(smallerName());
