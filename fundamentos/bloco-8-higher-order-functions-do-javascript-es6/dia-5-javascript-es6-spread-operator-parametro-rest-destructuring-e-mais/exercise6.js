103f870b141c35fb133e525559d0620a21a854da
fabianojustino <fibianojustino@gmail.com>
2022-01-24 20:41:17 -0300

    exercises 6

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise6.js
@@ -0,0 +1,9 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+
+const toObject = ([carro, marca, ano]) => ({ carro, marca, ano});
+
+console.log(toObject(palio));
